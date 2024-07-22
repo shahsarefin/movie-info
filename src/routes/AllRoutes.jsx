36712c3781165849f9daf462d2d2c1d295397b88
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-07-21 21:58:59 -0500

    feat: add routes for all pages including search and movie details

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -2,15 +2,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import { Movies, Movie, Search, PageNotFound } from '../pages';
+import { Home, Popular, TopRated, Upcoming, AllMovies, PageNotFound, MovieDetails, Search } from '../pages';
 
 export const AllRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Movies />} />
-      <Route path='/movie/:id' element={<Movie />} />
-      <Route path='/search' element={<Search />} />
-      <Route path='*' element={<PageNotFound />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/popular" element={<Popular />} />
+      <Route path="/top-rated" element={<TopRated />} />
+      <Route path="/upcoming" element={<Upcoming />} />
+      <Route path="/all-movies" element={<AllMovies />} />
+      <Route path="/movie/:id" element={<MovieDetails />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };
